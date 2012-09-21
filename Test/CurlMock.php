451806a2cce0dfc2721e05234f1dbeec8cf61b0f
Filippo De Santis <filippo.desantis@gmail.com>
2012-09-21 15:02:11 +0200

    Fixes FlickrApiTest::testGetSinglePhotoSet

--- Test/CurlMock.php
+++ Test/CurlMock.php
@@ -16,7 +16,7 @@ class CurlMock extends Curl
             case 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=a6d472134d5877b51a38070c7c631956&user_id=44774306@N00&photoset_id=72157623940754473&per_page=1&page=1':
                 return \file_get_contents(__DIR__.'/../DataFixtures/Files/photo_set_preview.xml');
                 break;
-            case 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=a6d472134d5877b51a38070c7c631956&user_id=44774306@N00&photoset_id=72157623940754473&extras=path_alias,url_sq,url_t,url_s,url_m,url_o':
+            case 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=a6d472134d5877b51a38070c7c631956&user_id=44774306@N00&photoset_id=72157623940754473&extras=path_alias,url_sq,url_t,url_s,url_l,url_m,url_o':
                 return \file_get_contents(__DIR__.'/../DataFixtures/Files/photo_set.xml');
                 break;
             case 'http://api.flickr.com/services/rest/?method=flickr.photosets.getInfo&api_key=a6d472134d5877b51a38070c7c631956&user_id=44774306@N00&photoset_id=72157623940754473':
